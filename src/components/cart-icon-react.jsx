90845f77c6b87c716c7804d3dd39cfb9e9abedd7
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-13 16:12:44 +0200

    Shoppingcart with badge

--- src/components/cart-icon-react.jsx
+++ src/components/cart-icon-react.jsx
@@ -25,6 +25,23 @@ export default class CartIconReact extends React.Component {
   }
 
   render() {
+    const badgecss = `
+    .fa-stack[data-count]:after{
+      position:absolute;
+      right:0%;
+      top:1%;
+      content: attr(data-count);
+      font-size:30%;
+      padding:.6em;
+      border-radius:999px;
+      line-height:.75em;
+      color: white;
+      background:rgba(255,0,0,.85);
+      text-align:center;
+      min-width:2em;
+      font-weight:bold;
+    }
+  `;
     return (
         <div>
           <style>
@@ -34,11 +51,14 @@ export default class CartIconReact extends React.Component {
             "https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css";
             @import
             "https://kit-free.fontawesome.com/releases/latest/css/free.min.css";
+            {badgecss}
+            }
           </style>
-          <a href="/shoppingcart">
-            <i class="fas fa-shopping-cart"></i>
+          <a className="fa-stack fa-2x has-badge"
+             href="/shoppingcart"
+             data-count={this.state.shoppingCarts.length}>
+            <i className="fa fa-shopping-cart fa-stack-1x"></i>
           </a>
-          ICON {this.state.shoppingCarts.length}
         </div>
     );
   }
