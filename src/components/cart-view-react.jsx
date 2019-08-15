ec334d0bb6b77ac2f3b4ef87bf3c57ba8be2284b
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-15 18:20:49 +0200

    correct binding

--- src/components/cart-view-react.jsx
+++ src/components/cart-view-react.jsx
@@ -32,7 +32,7 @@ export default class CartViewReact extends React.Component {
         <div>
           ShoppingcartView
           {this.state.cartItems.map(function (item) {
-            return <div>{{item.quantity}}</div>
+            return <div>{item.quantity}</div>
           })}
         </div>
     );
