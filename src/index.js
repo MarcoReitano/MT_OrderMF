0c83f310673bcd8dcb7f84ef8eacee9e1c7c6cc0
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-15 16:28:41 +0200

    Paths

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './components/order-view-component-react.jsx'
-import './components/cart-view-component-react.jsx'
+import './components/order-view--react.jsx'
+import './components/cart-view-react.jsx'
 import './components/cart-icon-react.jsx'
 import './components/add-cart-button-react.jsx'
